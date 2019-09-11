97877a2bbca7373e466196246870de27b35e9907
Darla <d.schub.54403@gmail.com>
2019-09-11 11:38:32 -0400

    Update ClassifiedsPage.js

--- church-bulletin-board/src/components/ClassifiedsPage.js
+++ church-bulletin-board/src/components/ClassifiedsPage.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import CreateAd from './CreateAd';
+
 
 export default class ClassifiedsPage extends Component {
     render() {
         return (
             <div>
                 <h1>You are on the Classifieds Page!</h1>
+                <CreateAd />
+                <p>this is where you place your ad</p>
+
             </div>
         )
     }
