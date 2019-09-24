24bb2620b5ec39be4621da788561ed1a5718733e
Cara <carawillaert@gmail.com>
2019-09-24 15:09:54 -0500

    modal form; hambuger functioning; new sticky note color

--- church-bulletin-board/src/components/Navbar.js
+++ church-bulletin-board/src/components/Navbar.js
@@ -6,23 +6,24 @@ export default class StickyNote extends Component {
 	render() {
 		return (
 			<div className='StickyNote'>
-				<nav className="navbar-light bg-dark navbar-expand-lg">
+				<nav className="navbar-dark bg-dark navbar-expand-lg">
 					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 						<span className="navbar-toggler-icon"></span>
 					</button>
 					<div className="collapse navbar-collapse" id="navbarTogglerDemo01">
 						<ul className="navbar-nav mx-auto">
+
 							<li className="navbar-item">
 								<Link to="/" className="nav-link">Home</Link>
 							</li>
 							<li className="navbar-item">
-								<Link to="./classifieds" className="nav-link">Classifieds</Link>												
+								<Link to="./classifieds" className="nav-link">Classifieds</Link>
 							</li>
 							<li className="navbar-item">
-								<Link to="./services" className="nav-link">Services</Link>					
+								<Link to="./services" className="nav-link">Services</Link>
 							</li>
 							<li className="navbar-item">
-								<Link to="./events" className="nav-link">Events</Link>					
+								<Link to="./events" className="nav-link">Events</Link>
 							</li>
 						</ul>
 					</div>
