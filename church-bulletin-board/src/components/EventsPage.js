051733209ce997d8d30998602d15f26c6a41bce7
Cara <carawillaert@gmail.com>
2019-10-05 17:52:48 -0500

    css updates

--- church-bulletin-board/src/components/EventsPage.js
+++ church-bulletin-board/src/components/EventsPage.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class EventsPage extends Component {
     render() {
         return (
+            <div className="eventspage">
             <div>
                 <h1>You are on the Events Page!</h1>
+            </div>
             </div>
         )
     }
--- church-bulletin-board/src/components/ServicesPage.js
+++ church-bulletin-board/src/components/ServicesPage.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class ServicesPage extends Component {
     render() {
         return (
+            <div className="servicespage">
             <div>
                 <h1>You are on the Services Page!</h1>
+            </div>
             </div>
         )
     }
